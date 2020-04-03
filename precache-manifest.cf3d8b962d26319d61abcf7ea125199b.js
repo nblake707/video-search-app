63fb7f6878bb76c1b089be7674f57e23b96cbf2e
Nateyana Blake <nateyanablake@MacBook-Pro-6.lan1>
2020-04-03 00:36:11 -0400

    Updates

---
+++ precache-manifest.cf3d8b962d26319d61abcf7ea125199b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0badc987d9b134b24bbdbbddc397f62",
+    "url": "/video-search-app/index.html"
+  },
+  {
+    "revision": "9cbcc6ab25556a22cdf1",
+    "url": "/video-search-app/static/css/main.d323dd02.chunk.css"
+  },
+  {
+    "revision": "adc118df9f529ed56b23",
+    "url": "/video-search-app/static/js/2.00816a6b.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/video-search-app/static/js/2.00816a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cbcc6ab25556a22cdf1",
+    "url": "/video-search-app/static/js/main.36a5ebfa.chunk.js"
+  },
+  {
+    "revision": "70a48b15a71d91291d1d",
+    "url": "/video-search-app/static/js/runtime-main.909b3f75.js"
+  }
+]);
